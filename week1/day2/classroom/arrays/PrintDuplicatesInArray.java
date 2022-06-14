54329d90b6a9f550882c8ab6d62a3fbd4a422e3e
Gayathrithirumalraj <107299750+Gayathrithirumalraj@users.noreply.github.com>
2022-06-14 08:48:59 -0700

    Update PrintDuplicatesInArray.java

--- week1/day2/classroom/arrays/PrintDuplicatesInArray.java
+++ week1/day2/classroom/arrays/PrintDuplicatesInArray.java
@@ -7,14 +7,20 @@ public class PrintDuplicatesInArray {
 		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
 		
 		// get the length of the array
-		// declare an int variable named count
+		Syste.out.println("DuplicatesInArray");
 		
+		// declare an int variable named count
+		for(int i=0;i<arr.length;i++){
+							
 		// iterate from 0 to the array length-1 (outer loop starts here)
 		
 			// assign 0 to count 
 			
+			for(int j=i+1;j<arr.length;j++){
 			// iterate from i to the length of the array by adding 1 to it (inner loop starts here)
-			
+			if(arr[i]==arr[j])
+				System.println(arr[j]);
+			}
 					// compare both the loop variables & check they're equal
 				
 							// increase the count if both the arrays are equal
